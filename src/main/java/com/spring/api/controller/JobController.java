3ccb1ed053abe8721120a3cd4b46ab16fa6c031c
EslamKhder <eslamkhder81@gmail.com>
2021-04-25 20:57:57 +0200

    JobController with ResponseEntity

--- src/main/java/com/spring/api/controller/JobController.java
+++ src/main/java/com/spring/api/controller/JobController.java
@@ -2,6 +2,8 @@ package com.spring.api.controller;
 
 import com.spring.api.model.Job;
 import com.spring.api.service.JobService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,33 +22,33 @@ public class JobController {
 
     // http://localhost:8080/api/jobs?page={value}&size={value}
     @GetMapping("/jobs")
-    public List<Job> allJobs(@RequestParam int page, @RequestParam int size){
-        return jobService.getAllJobs(page,size);
+    public ResponseEntity<List<Job>> allJobs(@RequestParam int page, @RequestParam int size){
+        return new ResponseEntity<List<Job>>(jobService.getAllJobs(page,size), HttpStatus.OK);
     }
 
     // http://localhost:8080/api/jobs
     @PostMapping("/jobs")
-    public Job addJob(@RequestBody Job job){
-        return jobService.addOrUpdateJob(job);
+    public ResponseEntity<Job> addJob(@RequestBody Job job){
+        return new ResponseEntity<Job>(jobService.addOrUpdateJob(job),HttpStatus.CREATED);
     }
 
     // http://localhost:8080/api/jobs?id={value}
     @PutMapping("/jobs")
-    public Job editJob(@RequestParam long id,@RequestBody Job job){
+    public ResponseEntity<Job> editJob(@RequestParam long id,@RequestBody Job job){
         job.setId(id);
-        return jobService.addOrUpdateJob(job);
+        return new ResponseEntity<Job>(jobService.addOrUpdateJob(job),HttpStatus.OK);
     }
 
     // http://localhost:8080/api/jobs/search?name={value}&page={value}&size={value}
     @GetMapping("jobs/search")
-    public List<Job> getJobByKey(@RequestParam String name,@RequestParam int page,@RequestParam int size){
-        return jobService.getJobsByKey(name,page,size);
+    public ResponseEntity<List<Job>> getJobByKey(@RequestParam String name,@RequestParam int page,@RequestParam int size){
+        return new ResponseEntity<List<Job>>(jobService.getJobsByKey(name,page,size),HttpStatus.OK);
     }
 
     // http://localhost:8080/api/jobs/status?id={value}?status=
     @PutMapping("jobs/status")
-    public int editStatus(@RequestParam int status,@RequestParam long id){
+    public ResponseEntity<Integer> editStatus(@RequestParam int status, @RequestParam long id){
         boolean active = (status ==1)? true : false;
-        return jobService.updateJobStatus(active,id);
+        return new ResponseEntity<Integer>(jobService.updateJobStatus(active,id),HttpStatus.OK);
     }
 }
